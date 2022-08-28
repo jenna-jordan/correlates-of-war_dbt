6a8a36012c26ba5b639daefc81df5cab40c1a770
Jenna Jordan <jenna.jrdn@gmail.com>
2022-08-28 17:07:38 -0400

    dbt init and duckdb loaded

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # correlates-of-war_dbt
 dbt project for the Correlates of War data
+
+## Environment set up
+
+```
+conda env create --name dbt-cow python=3.10
+pip install dbt-duckdb
+pip install duckcli
+pip install sqlfluff
+conda install pandas
+```
+
+## dbt project setup
+
+In repo root, ran:
+
+```
+dbt init
+correlates_of_war # Enter name of project
+1 # select duckdb as database
+```
+
---
+++ duckdb/create_db.py
@@ -0,0 +1,37 @@
+import duckdb
+import pandas as pd
+
+# these files don't use UTF-8 encoding, so I need to use pandas to read them in
+dfIntraStateWar = pd.read_csv(
+    "./duckdb/data/INTRA-STATE WARS v5.1 CSV.csv",
+    encoding="latin-1",
+    dtype=str,
+)
+dfExtraStateWar = pd.read_csv(
+    "./duckdb/data/Extra-StateWarData_v4.0.csv",
+    encoding="latin-1",
+    dtype=str,
+)
+
+
+conn = duckdb.connect("cow.db", read_only=False)
+
+table_creates = """
+DROP SCHEMA IF EXISTS sources CASCADE;
+CREATE SCHEMA sources;
+CREATE TABLE sources.states2016 AS 
+    SELECT * FROM read_csv_auto('./duckdb/data/states2016.csv', ALL_VARCHAR=TRUE);
+CREATE TABLE sources.system2016 AS 
+    SELECT * FROM read_csv_auto('./duckdb/data/system2016.csv', ALL_VARCHAR=TRUE);
+CREATE TABLE sources.interstatewar_v40 AS 
+    SELECT * FROM read_csv_auto('./duckdb/data/Inter-StateWarData_v4.0.csv', ALL_VARCHAR=TRUE);
+CREATE TABLE sources.nonstatewar_v40 AS 
+    SELECT * FROM read_csv_auto('./duckdb/data/Non-StateWarData_v4.0.csv', ALL_VARCHAR=TRUE);
+CREATE TABLE sources.intrastatewar_v51 AS 
+    SELECT * FROM dfIntraStateWar;
+CREATE TABLE sources.extrastatewar_v40 AS 
+    SELECT * FROM dfExtraStateWar;
+"""
+
+conn.execute(table_creates)
+conn.close()
